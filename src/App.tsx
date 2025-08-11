29625b093e119c8cea252c0c0fd425cfd7d3fd84
tetsuto-k <tkatsura@ucsb.edu>
2025-08-10 20:43:04 -0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from "react";
+import ReactDOM from "react-dom";
 import Draggable from 'react-draggable';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
