22f9e9c8f6279e8fd869a8d6bb3aab97b400d69c
tkatsura-personal <tkatsura@ucsb.edu>
2025-12-05 15:59:47 -0800

    Adding introduction

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import startPage from './pages/start';
 import NotFoundPage from './pages/notfound';
 
 function App() {
-  return (
+  return (<div>
+    <h1>Hello, and welcome to my board game!</h1>
+    <h2>Sadly, there's nothing here yet, but it's about to be filled with a game!</h2>
     <Routes>
       <Route path="/" element={<startPage />} />
       <Route path="/game" element={<gamePage />} />
       <Route path="/hub" element={<signUpPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
-  );
+    </div>
+    );
 }
 
 export default App;
