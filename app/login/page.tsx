6ddeedce0631fb51097563c2e05be30e5fa7fab6
tkatsura-personal <tkatsura@ucsb.edu>
2025-12-10 01:03:38 -0800

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,1 +1,12 @@
 //import { login } from "../lib/firebase/auth.ts";
+import React from "react";
+
+function page() {
+    return (
+        <div>
+            <h3>The page you are looking for could not be found :&#40;</h3>
+        </div>
+    );
+}
+
+export default page;
