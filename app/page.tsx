d8dd0b76751bd6c7fdf7414bf000fc18f8ce9539
tkatsura-personal <tkatsura@ucsb.edu>
2025-12-12 16:16:24 -0800

    Added button css

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,29 @@
 'use client';
-
-import { StrictMode } from 'react';
-import { useRouter } from 'next/router';
-import Signup from './client/signup';
-import Pagenotfound from "./client/notfound";
-import Login from './client/login';
+import { useRouter } from 'next/navigation';
 import Heading from "./Heading";
 import './globals.css';
 
 
 
 export default function Page() {
-  const router = useRouter()
+  const router = useRouter();
   
   return (
-    <StrictMode>
-      <div>
-        <Heading title = {"Welcome to my Rummikub Project!"}/>
-        <h1>Hello, and welcome to my board game!</h1>
-        <h3>Sadly, there's nothing here yet, but it's about to be filled with a game!</h3>
-        <Signup/>
-        <Pagenotfound/>
-        <Login/>
-      </div>
-    </StrictMode>
+    <div>
+      <Heading title = {"Welcome to my Rummikub Project!"}/>
+      <h1>Hello, and welcome to my board game!</h1>
+      <h3>Sadly, there's nothing here yet, but it's about to be filled with a game!</h3>
+      <button type="button" onClick={() => router.push('./client/game.tsx')}>
+        Play Game
+      </button>
+
+      <button type="button" onClick={() => router.push('./client/login.tsx')}>
+        Sign In
+      </button>
+
+      <button type="button" onClick={() => router.push('./client/signup.tsx')}>
+        Sign Up
+      </button>
+    </div>
   )
 }
