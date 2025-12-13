4569ecd014594f7a53aac660165779a271de8e1a
tkatsura-personal <tkatsura@ucsb.edu>
2025-12-12 16:47:23 -0800

    Added cosmetics

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,25 @@ export default function Page() {
       <Heading title = {"Welcome to my Rummikub Project!"}/>
       <h1>Hello, and welcome to my board game!</h1>
       <h3>Sadly, there's nothing here yet, but it's about to be filled with a game!</h3>
-      <button type="button" onClick={() => router.push('./client/game.tsx')}>
-        Play Game
+      <button type="button" className="button" onClick={() => router.push('./client/game.tsx')}>
+        Play Game 
+        <div className="arrow-wrapper">
+          <div className="arrow"></div>
+        </div>
       </button>
 
-      <button type="button" onClick={() => router.push('./client/login.tsx')}>
+      <button type="button" className="button" onClick={() => router.push('./client/login.tsx')}>
         Sign In
+        <div className="arrow-wrapper">
+          <div className="arrow"></div>
+        </div>
       </button>
 
-      <button type="button" onClick={() => router.push('./client/signup.tsx')}>
+      <button type="button" className="button" onClick={() => router.push('./client/signup.tsx')}>
         Sign Up
+        <div className="arrow-wrapper">
+          <div className="arrow"></div>
+        </div>
       </button>
     </div>
   )
