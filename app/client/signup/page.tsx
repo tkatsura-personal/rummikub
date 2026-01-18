78966292757fe82d38e03321b1ff33ee8351d734
tkatsura-personal <tkatsura@ucsb.edu>
2026-01-17 21:11:23 -0800

    renamed config name

--- app/client/signup/page.tsx
+++ app/client/signup/page.tsx
@@ -9,13 +9,13 @@ import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail, delete
 import { getFirestore, doc, collection, getDocs, setDoc, serverTimestamp } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.CLIENT_FIREBASE_WEB_API_KEY,
-  authDomain: process.env.CLIENT_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.CLIENT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.CLIENT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.CLIENT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.CLIENT_FIREBASE_APP_ID,
-  measurementID:process.env.CLIENT_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_WEB_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
